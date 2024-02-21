f280876a919806982072c6acab428fb7a15b6a81
stevehndz <steve.r.hndz@gmail.com>
2024-02-21 12:16:28 -0600

    fix(type): Update file type.ts

--- build/type.js
+++ build/type.js
@@ -7,3 +7,35 @@ let age = 12;
 let firstName = 'Steve';
 let lastName = 'Rodas';
 let fullName = `${firstName} ${lastName}`;
+// Array
+let numbers1 = [12, 3, 4, 14, 5, 6];
+let numbers2 = [3, 1, 65, 2];
+// Tupla
+let site = ['Casa', 28008];
+// Enum
+var Status;
+(function (Status) {
+    Status[Status["Offline"] = 0] = "Offline";
+    Status[Status["Undefined"] = 1] = "Undefined";
+    Status[Status["Online"] = 2] = "Online"; // 2
+})(Status || (Status = {}));
+/* enum Status {
+    Offline = -1,
+    Undefined = 0,
+    Online = 1
+}*/
+let stat = Status.Online;
+console.log(stat);
+// Unknown
+let withoutType = 'Hola';
+withoutType = 32;
+withoutType = true;
+let newString = 'Esto es una cadena';
+// newString = withoutType;
+// Any
+let undefinedType = 'Esto es un mensaje';
+newString = undefinedType;
+// Void
+function logger() {
+    console.log('Logger');
+}
--- type.ts
+++ type.ts
@@ -10,3 +10,44 @@ let firstName: string = 'Steve';
 let lastName: string = 'Rodas';
 
 let fullName: string = `${firstName} ${lastName}`;
+
+// Array
+let numbers1: number[] = [12,3,4,14,5,6];
+let numbers2: Array<number> = [3,1,65,2];
+
+// Tupla
+let site: [string, number] = ['Casa', 28008];
+
+// Enum
+enum Status {
+    Offline, // 0
+    Undefined, // 1
+    Online // 2
+}
+
+/* enum Status {
+    Offline = -1,
+    Undefined = 0,
+    Online = 1
+}*/
+
+let stat: Status = Status.Online;
+console.log(stat);
+
+// Unknown
+let withoutType: unknown = 'Hola';
+withoutType = 32;
+withoutType = true;
+
+let newString: string = 'Esto es una cadena';
+// newString = withoutType;
+
+// Any
+let undefinedType: any = 'Esto es un mensaje';
+newString = undefinedType;
+
+// Void
+function logger(): void {
+    console.log('Logger');
+    
+}
