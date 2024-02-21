2999df0583fede97fded10adb690b3824cc729d1
stevehndz <steve.r.hndz@gmail.com>
2024-02-21 12:24:59 -0600

    fix(interfaces): added function Car

--- build/interfaces.js
+++ build/interfaces.js
@@ -2,3 +2,14 @@ function showPerson(pPerson) {
     console.log(pPerson);
 }
 showPerson({ firstName: 'Beatriz', lastName: 'Perez', age: 20 });
+function showCar(pCar) {
+    console.log(`Marca: ${pCar.brand}. Modelo: ${pCar.model}`);
+    if (pCar.color) {
+        console.log(`Color: ${pCar.color}`);
+    }
+    else {
+        console.log('El coche no tiene color');
+    }
+}
+showCar({ brand: 'Mercedes', model: 'C180' });
+showCar({ brand: 'Mercedes', model: 'TY121', color: 'Rojo' });
--- interfaces.ts
+++ interfaces.ts
@@ -9,3 +9,24 @@ function showPerson(pPerson: Person): void {
 }
 
 showPerson({firstName: 'Beatriz', lastName: 'Perez', age: 20});
+
+interface Car {
+    brand: string;
+    model: string;
+    color?: string; // Not required
+}
+
+function showCar(pCar: Car) {
+    console.log(`Marca: ${pCar.brand}. Modelo: ${pCar.model}`);
+    
+    if(pCar.color) {
+        console.log(`Color: ${pCar.color}`);
+    }
+    else {
+        console.log('El coche no tiene color');
+        
+    }
+}
+
+showCar({brand: 'Mercedes', model: 'C180'});
+showCar({brand: 'Mercedes', model: 'TY121', color: 'Rojo'});
